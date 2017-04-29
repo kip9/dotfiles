c43d1548bc865e9607ce4001d87260c566d190f8
Jakub Jakubiec <jjakubiec@gmail.com>
2017-04-29 22:25:54 +0200

    Add setup script. Update README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# dotfiles
+# dotfiles by kip9
+## Usage
+1. Clone into ~/.dotfiles/
+2. Run ~/.dotfiles/bin/setup.sh
+
+## Info
+Inspired by: https://github.com/cowboy/dotfiles
---
+++ bin/setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+export DOTFILES_LOCATION=~/.dotfiles
+backup_dir=$DOTFILES_LOCATION/backups/$(date "+%Y_%m_%d-%H_%M_%S")/
+
+function print_info() {
+  echo "$1"
+}
+
+function link_file() {
+  print_info "Linking $1"
+  ln -sf ${1#$HOME/} ~/
+}
+
+function process_files() {
+  local filename destination
+  local files=($DOTFILES_LOCATION/links/*)
+  if (( ${#files[@]} == 0 )); then return; fi
+  for file in "${files[@]}"; do
+    filename="$(basename $file)"
+    destination="$HOME/$filename"
+    if [[ -e "$destination" ]]; then
+      print_info "Making backup of $destination"
+      [[ -e "$backup_dir" ]] || mkdir -p "$backup_dir"
+      # Backup file / link / whatever.
+      mv "$destination" "$backup_dir"
+    fi
+    link_file "$file"
+  done
+}
+
+# Main code
+
+while true; do sudo -n true; sleep 10; kill -0 "$$" || exit; done 2>/dev/null &
+
+shopt -s dotglob
+shopt -s nullglob
+
+process_files
